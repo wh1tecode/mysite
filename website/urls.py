dce11d2ff03a637fe41147132a8020368f3554f8
khosro <khosro@local.com>
2024-05-06 19:49:36 +0330

    Append templates to project

--- website/urls.py
+++ website/urls.py
@@ -3,7 +3,7 @@ from website.views import home_view, about_view, contact_view
 
 
 urlpatterns = [
-    path('home', home_view),
+    path('', home_view),
     path('about', about_view),
     path('contact', contact_view),
 ]
--- website/views.py
+++ website/views.py
@@ -5,7 +5,7 @@ from django.http import JsonResponse, HttpResponse
 # Create your views here.
 
 def home_view(request):
-    return JsonResponse({"Home": 1})
+    return render(request, "index.html")
 
 def about_view(request):
     return JsonResponse({"About": 2})

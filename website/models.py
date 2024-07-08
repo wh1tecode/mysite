ebc602d6975355f6c8407d903c901164e0af40d7
khosro <khsro@gmail.com>
2024-07-08 23:06:25 +0330

    Create language filed for about and Blog models

--- website/models.py
+++ website/models.py
@@ -13,8 +13,12 @@ class User(models.Model):
     url_address = models.CharField(max_length=32, default=str(uuid4().hex))
     birth_date = models.DateField()
     age = models.IntegerField()
-    avatar_image = models.ImageField(default="photo", upload_to="photos/%Y/%m/%d", blank=True, null=True)
-    banner_image = models.ImageField(default="photo", upload_to="photos/%Y/%m/%d", blank=True, null=True)
+    avatar_image = models.ImageField(
+        default="photo", upload_to="photos/%Y/%m/%d", blank=True, null=True
+    )
+    banner_image = models.ImageField(
+        default="photo", upload_to="photos/%Y/%m/%d", blank=True, null=True
+    )
     created_date = models.DateField(auto_now_add=True)
     updated_date = models.DateField(auto_now=True)
     admin = models.ForeignKey(AdminUser, on_delete=models.CASCADE)
@@ -33,6 +37,7 @@ class About(models.Model):
     city = models.CharField(max_length=20)
     degree = models.CharField(max_length=20)
     freelance = models.BooleanField()
+    language = models.CharField(max_length=250)
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
 
 
@@ -62,9 +67,13 @@ class Experience(models.Model):
 
 
 class Project(models.Model):
-    name = models.CharField(max_length=256, choices=[("api", "api"), ("web", "web"), ("android", "android")])
+    name = models.CharField(
+        max_length=256, choices=[("api", "api"), ("web", "web"), ("android", "android")]
+    )
     url = models.URLField(blank=True)
-    image = models.ImageField(default="photo", upload_to="photos/%Y/%m/%d", blank=True, null=True)
+    image = models.ImageField(
+        default="photo", upload_to="photos/%Y/%m/%d", blank=True, null=True
+    )
     description = models.TextField()
     repository = models.CharField(max_length=50, default=None)
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -77,3 +86,39 @@ class Contact(models.Model):
     message = models.TextField()
     created_date = models.DateField(auto_now_add=True)
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=30)
+    image = models.ImageField(
+        default="photo", upload_to="photos/%Y/%m/%d", blank=True, null=True
+    )
+    content = models.TextField()
+    published_date = models.DateField()
+    created_date = models.DateField(auto_now_add=True)
+    updated_date = models.DateField(auto_now_add=True)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+class PostComment(models.Model):
+    content = models.TextField()
+    created_date = models.DateField(auto_now_add=True)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+
+
+class PostView(models.Model):
+    created_date = models.DateField(auto_now_add=True)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+
+
+class PostLike(models.Model):
+    created_date = models.DateField(auto_now_add=True)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+
+
+class PostCategory(models.Model):
+    name = models.CharField(max_length=20)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
